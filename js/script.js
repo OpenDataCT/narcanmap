b3b3a015913a568d9d87d15a570525d552739d4e
ConnecticutCDO <tyler.kleykamp@ct.gov>
2016-05-14 11:09:08 -0400

    working SODA nearest query

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 
 $(document).ready(function () {
-    var sevenDaysAgo;
+    var mapnarcan;
     //initialize the leaflet map, set options and view
-    var map = L.map('map', {
-        zoomControl: false,
-        scrollWheelZoom: false
-    })
-	.setView([40.705008, -73.995581], 15);
+    var map = L.map('map');
 
-    var markers = new L.FeatureGroup();
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(map);
 
-    //add an OSM tileset as the base layer
-    L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-    }).addTo(map);
+		function onLocationFound(e) {
+			var radius = e.accuracy / 2;
+
+			L.marker(e.latlng).addTo(map)
+				.bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+			L.circle(e.latlng, radius).addTo(map);
+		}
+
+		function onLocationError(e) {
+			alert(e.message);
+		}
+
+		map.on('locationfound', onLocationFound);
+		map.on('locationerror', onLocationError);
+
+		map.locate({setView: true, maxZoom: 16});
 
     //call our getData() function.
     getData();
@@ -31,63 +46,23 @@ $(document).ready(function () {
         }
     });
 
-    //define agency icons based on the base icon
-    var tlcIcon = new baseIcon({ iconUrl: 'img/taxi.png' });
-    var dotIcon = new baseIcon({ iconUrl: 'img/dot.png' });
-    var parksIcon = new baseIcon({ iconUrl: 'img/parks.png' });
-    var buildingsIcon = new baseIcon({ iconUrl: 'img/buildings.png' });
-    var nypdIcon = new baseIcon({ iconUrl: 'img/nypd.png' });
-    var dsnyIcon = new baseIcon({ iconUrl: 'img/dsny.png' });
-    var fdnyIcon = new baseIcon({ iconUrl: 'img/fdny.png' });
-    var doeIcon = new baseIcon({ iconUrl: 'img/doe.png' });
-    var depIcon = new baseIcon({ iconUrl: 'img/dep.png' });
-    var dofIcon = new baseIcon({ iconUrl: 'img/dof.png' });
-    var dcaIcon = new baseIcon({ iconUrl: 'img/dca.png' });
-    var dohmhIcon = new baseIcon({ iconUrl: 'img/dohmh.png' });
-    var hpdIcon = new baseIcon({ iconUrl: 'img/hpd.png' });
-
+   
 
     function getData() {
-        //get map bounds from Leaflet
-        var bbox = map.getBounds();
-        //map.removeLayer(markers);
-        markers.clearLayers();
-        //create a SODA-ready bounding box that looks like: topLeftLat,topLeftLon,bottomRightLat,bottomRightLon
-        var sodaQueryBox = [bbox._northEast.lat, bbox._southWest.lng, bbox._southWest.lat, bbox._northEast.lng];
-
-        //figure out what the date was 7 days ago
-        var sevenDaysAgo = new Date();
-        sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
-
-        $('#startDate').html(sevenDaysAgo.toDateString());
-
-        function cleanDate(input) {
-            return (input < 10) ? '0' + input : input;
-        }
-
-        //create a SODA-ready date string that looks like: 2014-11-01
-        sevenDaysAgo = sevenDaysAgo.getFullYear()
-			+ '-'
-			+ cleanDate((sevenDaysAgo.getMonth() + 1))
-			+ '-'
-			+ cleanDate((sevenDaysAgo.getDate() + 1));
-
-        //use jQuery's getJSON() to call the SODA API for NYC 311
-        //concatenate sodaQueryBox and sevenDaysAgo to add a $where clause to the SODA endpoint
-        $.getJSON(constructQuery(sevenDaysAgo, sodaQueryBox), function (data) {
+        
+        $.getJSON(constructQuery(sodaQuery), function (data) {
 
                 console.log(data)
 			    //iterate over each 311 complaint, add a marker to the map
 			    for (var i = 0; i < data.length; i++) {
 
 			        var marker = data[i];
-			        var icon = getIcon(marker);
+			        var icon = getIcon(baseIcon);
 
 			        var markerItem = L.marker([marker.location.latitude, marker.location.longitude], { icon: icon });
 			        markerItem.bindPopup(
-							'<h4>' + marker.complaint_type + '</h4>'
-							+ (new Date(marker.created_date)).toDateString()
-							+ ((marker.incident_address != null) ? '<br/>' + marker.incident_address : '')
+							'<h4>' + marker.pharmacy + '</h4>'
+							+ ((marker.address) ? '<br/>' + marker.email_fax_other : '')
 						);
 			        markers.addLayer(markerItem);
 			    }
@@ -97,59 +72,12 @@ $(document).ready(function () {
 			})
     }
 
-    function constructQuery(sevenDaysAgo, sodaQueryBox) {
-        var originalstr = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$select=location,closed_date,complaint_type,street_name,created_date,status,unique_key,agency_name,due_date,descriptor,location_type,agency,incident_address&$where=created_date>'"
-			+ sevenDaysAgo
-			+ "' AND within_box(location,"
-			+ sodaQueryBox
-			+ ")&$order=created_date desc"
-
-        var agency = $( "#nycAgency" ).val();
-        var conditiion = $("#conditions_list").val();
-        if (agency.length != 0 && agency != "All") {
-            originalstr = originalstr + "&agency=" + agency;
-        }
-        if (conditiion.length != 0 && conditiion != "All") {
-            originalstr = originalstr + "&complaint_type=" + conditiion;
-        }
-
-        console.log(originalstr);
+    function constructQuery(sodaQuery) {
+        var originalstr = "https://data.ct.gov/resource/wzna-yuqm.json?$select=*,DISTANCE_IN_METERS(location,'POINT(location.latitude,location.longitude)AS distance_in_meters&$limit=5&$order=distance_in_meters asc,pharmacy,address,email_fax_other>'"
 
         return originalstr;
     }
-    function getIcon(thisMarker) {
-
-        switch (thisMarker.agency) {
-            case 'TLC':
-                return tlcIcon;
-            case 'DOT':
-                return dotIcon;
-            case 'DPR':
-                return parksIcon;
-            case 'DOB':
-                return buildingsIcon;
-            case 'NYPD':
-                return nypdIcon;
-            case 'DSNY':
-                return dsnyIcon;
-            case 'FDNY':
-                return fdnyIcon;
-            case 'DOE':
-                return doeIcon;
-            case 'DEP':
-                return depIcon;
-            case 'DOF':
-                return dofIcon;
-            case 'DCA':
-                return dcaIcon;
-            case 'DOHMH':
-                return dohmhIcon;
-            case 'HPD':
-                return hpdIcon;
-            default:
-                return new L.Icon.Default();
-        }
-    }
+    
 
     map.on('dragend', function (e) {
         getData();
