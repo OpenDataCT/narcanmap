00a3457233443f5244d5aa60f1fd8e6cff83768b
Tyler Kleykamp <OpenDataCT@users.noreply.github.com>
2017-04-27 11:11:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@
     //initialize the leaflet map, set options and view
     var map = L.map('map');
 
-		L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/{light_all}/{z}/{x}/{y}.png', {
+		L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
 	maxZoom: 18,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a><a href="http://cartodb.com/attributions">CartoDB</a>'
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="http://cartodb.com/attributions">CartoDB</a>'
 
 		}).addTo(map);
 
