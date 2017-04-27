42201297024ab8fdd99c6812d42723f96ea7a431
Tyler Kleykamp <OpenDataCT@users.noreply.github.com>
2017-04-27 11:14:46 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,7 @@
     var map = L.map('map');
 
 		L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-        maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;
-        <a href="https://carto.com/attribution">CARTO</a>'
+        maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
 
 		}).addTo(map);
 
