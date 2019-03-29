557677eeab555168d2afc861482fe460fe5f34d5
Tyler Kleykamp <OpenDataCT@users.noreply.github.com>
2019-03-29 11:10:08 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@
     //initialize the leaflet map, set options and view
     var map = L.map('map');
 
-		L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+		L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
 		}).addTo(map);
 
